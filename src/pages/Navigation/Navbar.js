2ea762689139ed23b9f32eed3cf10dc3d3b37e74
JuliCarracedo <julicarra@hotmail.com>
2022-02-25 19:17:55 -0300

    style nav-links

--- src/pages/Navigation/Navbar.js
+++ src/pages/Navigation/Navbar.js
@@ -1,19 +1,18 @@
-import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import '../../styles/navbar.css'
 const Navbar = () => {
     return(
         <nav className='navbar'>
             <div>
-                <NavLink to='/' >Home</NavLink>
-                <NavLink to='/' >Creators</NavLink>
-                <NavLink to='/' >Categories</NavLink>
-                <NavLink to='/' >Discover</NavLink>
+                <NavLink className='link' to='/' >Home</NavLink>
+                <NavLink className='link' to='/creators' >Creators</NavLink>
+                <NavLink className='link' to='/categories' >Categories</NavLink>
+                <NavLink className='link' to='/discover' >Discover</NavLink>
             </div>
             <input type='text'/>
             <div>
-                <NavLink to='/' >Log In</NavLink>
-                <NavLink to='/' >Sign up</NavLink>
+                <NavLink className='link' to='/login' >Log In</NavLink>
+                <NavLink className='link' to='/signup' >Sign up</NavLink>
             </div>
         </nav>
     )
