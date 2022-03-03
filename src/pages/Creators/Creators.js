871c26f2914ad3facae4d8af2ad4298f12e468e8
JuliCarracedo <julicarra@hotmail.com>
2022-03-03 16:28:50 -0300

    Add creators component

---
+++ src/pages/Creators/Creators.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Creators = () => {
+    const creators = ['GrandAlchemist', 'Mazahi Kishimoto', 'Lee Stan', 'ComicMaster', 'PencilManiac', '']
+    
+    return (
+        <section>
+            <h2>Creators</h2>
+
+            
+        </section>
+    )
+}
