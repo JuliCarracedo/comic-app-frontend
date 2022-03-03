596f9da5e5fdfc4853dda64435460e2bb14192a8
JuliCarracedo <julicarra@hotmail.com>
2022-03-03 16:13:49 -0300

    fix carousel selector styles

--- src/pages/Homepage/CarouselSelector.js
+++ src/pages/Homepage/CarouselSelector.js
@@ -12,8 +12,9 @@ const CarouselSelector = () => {
         <div className="carousel-selector">
             <ul>
                {categories.map((cat, i) => (<li key={cat} className={
-                    (i != 0 ? 'not-first' : 'first').concat(cat  === active ? ' active-select' : '') }>
-                    <div onClick={e=>handleClick(e,cat)}>
+                    (i != 0 ? 'not-first' : 'first').concat(cat  === active ? ' active-select' : '')}
+                    onClick={e=>handleClick(e,cat)}>
+                    <div>
                         <h3>{cat}</h3>
                     </div>
                 </li>))}
