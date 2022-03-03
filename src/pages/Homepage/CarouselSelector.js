74bb8a481d43e53c74eec51d939e0b5146fae717
JuliCarracedo <julicarra@hotmail.com>
2022-03-03 16:08:12 -0300

    Add selected status to carousel selector elements

--- src/pages/Homepage/CarouselSelector.js
+++ src/pages/Homepage/CarouselSelector.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../styles/carouselSelector.css'
 const CarouselSelector = () => {
 
     const categories = ['TOP', 'ACTION', 'ROMANCE', 'FANTASY'];
-
+    const [active,setActive] = useState('TOP')
+    const handleClick = (e, value) => {
+        e.preventDefault()
+        setActive(value)
+    }
     return (
         <div className="carousel-selector">
             <ul>
-               {categories.map((cat, i) => (<li key={cat} className={ i != 0 ? 'not-first' : 'first' }>
-                    <div>
+               {categories.map((cat, i) => (<li key={cat} className={
+                    (i != 0 ? 'not-first' : 'first').concat(cat  === active ? ' active-select' : '') }>
+                    <div onClick={e=>handleClick(e,cat)}>
                         <h3>{cat}</h3>
                     </div>
                 </li>))}
