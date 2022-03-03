f62643f6d0e3956cc5855b1f764800609e7aa74a
JuliCarracedo <julicarra@hotmail.com>
2022-03-03 15:54:05 -0300

    add support for online images

--- src/pages/Homepage/CarouselComponent.js
+++ src/pages/Homepage/CarouselComponent.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 import '../../styles/carousel.css'
 const CarouselComponent = () =>{
-  const content = ['Lorem ipsum', 'Title', 'Hello World']
+  const alt = ['dragonball', 'naruto', 'bleach']
+  const image = ['https://dragonline86.files.wordpress.com/2008/10/155jpg.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpPv6z69QcXX4E5hk1nBLSLosRQYSqZ1b4Qg&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2nj4bhVn36GwOSSIxTDK1ICAyTGu39lGSMg&usqp=CAU']
   const [current, setCurrent] = useState(0)
 
   const back = () =>{
-    setCurrent(current === 0 ? content.length -1 : current - 1)
+    setCurrent(current === 0 ? alt.length -1 : current - 1)
   }
 
   const forward = () =>{
-    setCurrent(current === content.length -1 ? 0 : current + 1)
+    setCurrent(current === alt.length -1 ? 0 : current + 1)
   }
 
   return(
     <div className="carousel-container">
       <button type="button" onClick={back}> {'<'} </button>
       <div className="carousel-inner">
-          {<div className="item">
-                {content[current]}
-          </div>}
+          {<a className="item"><img className="item" key={alt[current]} alt={alt[current]} src={image[current]} /></a>}
       </div>
       <button type="button" onClick={forward}> {'>'} </button>
     </div>
