93006e2a93eb1714954fe24d8525804f1607dceb
JuliCarracedo <julicarra@hotmail.com>
2022-03-07 13:02:21 -0300

    Finish comic page wireframe

--- src/pages/Comic/Comic.js
+++ src/pages/Comic/Comic.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '../../styles/comic.css'
 const Comic = () => {
     const chapters = [{number: 1, title: 'Furst Chaprer', description: 'Loremm ipsum dolor sit amet'},
                     {number: 4, title: 'Furt Chaprer', description: 'Loremm ipsum dolor sit amet'}, 
@@ -10,8 +10,13 @@ const Comic = () => {
      return (
         <div className="container">
             <h2>Comic Title</h2>
-            <p> X Likes </p>
-            <p> X Followers </p>
+            <h3> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus erat ac mauris volutpat facilisis. Fusce malesuada ex in nisl ornare, at ornare augue condimentum. Nullam mattis nisi id dui.</h3>
+            <div className="stats">
+                <p> X Likes </p>
+                <p> X Followers </p>
+            </div>
+            <div className="comic-body">
+            
             <ul className="chapters-list">
                 {
                    chapters.sort((a,b)=>a.number < b.number ? -1 : 1).map(
@@ -23,6 +28,20 @@ const Comic = () => {
                    ) 
                 }
             </ul> 
+
+            <ul className="similar-comics">
+                <li><h3>Similar comics</h3></li>
+                {
+                   chapters.sort((a,b)=>a.number < b.number ? -1 : 1).map(
+                       chapter => < li key={chapter.number}>
+                           <h3>{chapter.title}</h3> 
+                           <p>{chapter.description}</p>
+                           <p> x Likes</p>
+                           </li>
+                   ) 
+                }
+            </ul> 
+            </div>
         </div>
      )
 }
