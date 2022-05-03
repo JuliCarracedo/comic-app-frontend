64cb4590a2b5b0920710afe52c6296251d03b890
JuliCarracedo <julicarra@hotmail.com>
2022-05-03 18:45:18 -0300

    Create RegisterThunk

--- src/pages/User/Register.js
+++ src/pages/User/Register.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import loginThunk from "../../redux/users/loginThunk";
 
 const Register = () => {
 
