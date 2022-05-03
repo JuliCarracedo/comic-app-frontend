10832798eec33a1647747c734fe0812f7b4b2b17
JuliCarracedo <julicarra@hotmail.com>
2022-05-03 19:02:47 -0300

    Add actual username to navbar

--- src/pages/Navigation/Navbar.js
+++ src/pages/Navigation/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import '../../styles/navbar.css'
 const Navbar = () => {
 
-    const { logged } = useSelector(store=>store.user)
+    const { logged, user } = useSelector(store=>store.user)
 
     return(
         <nav className='navbar'>
@@ -16,7 +16,7 @@ const Navbar = () => {
             <input type='text'/>
             {logged ? <div>
                 <NavLink className='link' to='/' >Log Out</NavLink>
-                <NavLink className='link' to='/profile' > Username </NavLink>
+                <NavLink className='link' to='/profile' > {user.username} </NavLink>
             </div> : <div>
                 <NavLink className='link' to='/login' >Log In</NavLink>
                 <NavLink className='link' to='/signup' >Sign up</NavLink>
