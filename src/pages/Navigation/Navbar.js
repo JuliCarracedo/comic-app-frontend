450241c62bf3c80400ec5c56e3ae3b48e05dfc4d
JuliCarracedo <julicarra@hotmail.com>
2022-05-03 17:26:14 -0300

    Include interface for logged in users

--- src/pages/Navigation/Navbar.js
+++ src/pages/Navigation/Navbar.js
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import '../../styles/navbar.css'
 const Navbar = () => {
+
+    const { logged } = useSelector(store=>store.user)
+
     return(
         <nav className='navbar'>
             <div>
@@ -10,10 +14,16 @@ const Navbar = () => {
                 <NavLink className='link' to='/discover' >Discover</NavLink>
             </div>
             <input type='text'/>
-            <div>
+            {logged ? <div>
                 <NavLink className='link' to='/login' >Log In</NavLink>
                 <NavLink className='link' to='/signup' >Sign up</NavLink>
-            </div>
+            </div> : <div>
+                <NavLink className='link' to='/' >Log Out</NavLink>
+                <NavLink className='link' to='/profile' >
+                    <h1> Username </h1>
+                    <img alt="profile" src="https://thumbs.dreamstime.com/b/default-avatar-profile-image-vector-social-media-user-icon-potrait-182347582.jpg"/>
+                </NavLink>
+                </div>}
         </nav>
     )
 }
