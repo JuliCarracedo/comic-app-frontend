c75e60992530aae63c92774a0af5d19bb86c9afa
JuliCarracedo <julicarra@hotmail.com>
2022-05-04 13:15:09 -0300

    Add logout action on navbar

--- src/pages/Navigation/Navbar.js
+++ src/pages/Navigation/Navbar.js
@@ -1,10 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { logout } from "../../redux/users/usersReducer";
 import '../../styles/navbar.css'
 const Navbar = () => {
 
     const { logged, user } = useSelector(store=>store.user)
+    const dispatch = useDispatch()
 
+    const clickLogOut = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+        localStorage.removeItem('token');
+    }
     return(
         <nav className='navbar'>
             <div>
@@ -15,7 +22,7 @@ const Navbar = () => {
             </div>
             <input type='text'/>
             {logged ? <div>
-                <NavLink className='link' to='/' >Log Out</NavLink>
+                <NavLink className='link' to='/' onClick={e=>clickLogOut(e)} >Log Out</NavLink>
                 <NavLink className='link' to='/profile' > {user.username} </NavLink>
             </div> : <div>
                 <NavLink className='link' to='/login' >Log In</NavLink>
