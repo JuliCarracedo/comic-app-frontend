66a04f32149114df8f98885b8d530decc4322251
JuliCarracedo <julicarra@hotmail.com>
2022-05-04 13:11:54 -0300

    Add logout to users Reducer

--- src/redux/users/usersReducer.js
+++ src/redux/users/usersReducer.js
@@ -1,23 +1,27 @@
 // CONSTANTS
 
-const REQUEST_LOGIN = "redux/usersReducer/REQUEST_LOGIN"
-const LOGIN_CONFIRMED = "redux/usersReducer/LOGIN_CONFIRMED"
-const LOGIN_FAILED = "redux/usersReducer/LOGIN_FAILED"
+const REQUEST_LOGIN = "redux/usersReducer/REQUEST_LOGIN";
+const LOGIN_CONFIRMED = "redux/usersReducer/LOGIN_CONFIRMED";
+const LOGIN_FAILED = "redux/usersReducer/LOGIN_FAILED";
 
-const REQUEST_REGISTRATION = "redux/usersReducer/REQUEST_REGISTRATION"
-const REGISTER_CONFIRMED = "redux/usersReducer/REGISTER_CONFIRMED"
-const REGISTER_FAILED = "redux/usersReducer/REGISTER_FAILED"
+const REQUEST_REGISTRATION = "redux/usersReducer/REQUEST_REGISTRATION";
+const REGISTER_CONFIRMED = "redux/usersReducer/REGISTER_CONFIRMED";
+const REGISTER_FAILED = "redux/usersReducer/REGISTER_FAILED";
 
-const initialState = {}
+const LOGOUT = "redux/usersReducer/LOGOUT";
+
+const initialState = {};
 // ACTIONS
 
-export const requestLogin = () => ({ type: REQUEST_LOGIN})
-export const loginConfirmed = (user) => ({ type: LOGIN_CONFIRMED, user})
-export const loginFailed = () => ({ type: LOGIN_FAILED})
+export const requestLogin = () => ({ type: REQUEST_LOGIN});
+export const loginConfirmed = (user) => ({ type: LOGIN_CONFIRMED, user});
+export const loginFailed = () => ({ type: LOGIN_FAILED});
 
-export const requestRegistration = () => ({ type: REQUEST_REGISTRATION})
-export const registerConfirmed = (user) => ({ type: REGISTER_CONFIRMED, user})
-export const registerFailed = () => ({ type: REGISTER_FAILED})
+export const requestRegistration = () => ({ type: REQUEST_REGISTRATION});
+export const registerConfirmed = (user) => ({ type: REGISTER_CONFIRMED, user});
+export const registerFailed = () => ({ type: REGISTER_FAILED});
+
+export const logout = () => ({type: LOGOUT});
 
 const usersReducer = (state = initialState , action) => {
     switch(action.type){
@@ -29,6 +33,9 @@ const usersReducer = (state = initialState , action) => {
         case REQUEST_REGISTRATION: return {...state, loading: true}
         case REGISTER_FAILED: return {...state, loading: false}
         case REGISTER_CONFIRMED: return {...state, loading: false, logged: true, user: action.user}
+        // Log out
+        case LOGOUT: return {};
+        
         default: return state // We return the default state here
     }
 }
