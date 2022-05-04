a37d78c73039fa15d6447bad2150528ce6b7f359
JuliCarracedo <julicarra@hotmail.com>
2022-05-04 12:04:44 -0300

    Create special notification backgrounds

--- src/pages/Notice/NoticeComponent.js
+++ src/pages/Notice/NoticeComponent.js
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
+import "../../styles/notice.css";
 
 const NoticeComponent = ( ) => {
 
-    const [alive, setAlive] = useState(true)
+    const [alive, setAlive] = useState(false);
+    // const [type, setType] = useState('idle');
+
+    // const {}
 
     useEffect(()=>{
-        setTimeout(setAlive,10000,false);
-    },[]) 
+        setTimeout(setAlive, 10000, false);
+    },[]); 
 
     const killNotice = (e) => {
         e.preventDefault();
         setAlive(false);
     }
 
-    return(alive && <div className="notice">
+    return(alive && <div className="notice message">
         <p>You are being notified</p>
         <button type="button" onClick={e=>killNotice(e)}>x</button>
     </div>)
