5bc760c596e678b8254c89f1ac11af5158c11016
JuliCarracedo <julicarra@hotmail.com>
2022-05-09 11:55:23 -0300

    Properly input created user and set up profile import

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,32 +1,31 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import ComicsCard from "../Comic-Card/ComicsCard";
+// import ComicsCard from "../Comic-Card/ComicsCard";
 
 const User = () => {
 
     const { search } = useSelector(state=>state.user)
-    const user = search.user || false;
 
-    const comics = [{category: 'Action', title: 'Dragon Ball'},
-    {category: 'Action', title: 'Naruto'},
-    {category: 'Fantasy', title: 'Tales of a Swordsman'}]
+    const fetchImage = () =>{
+        return 'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80'
+    }    
 
-    return( user ?
+    return( search ?
     <div className="container">
         
         <div className="top">
-            <img alt="profile" className="profile-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDyvGPdkQdUyozfQIHwKd6vBUg0gF7w-mpiQ&usqp=CAU"/>
+            <img alt="profile" className="profile-img" src={fetchImage() || 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDyvGPdkQdUyozfQIHwKd6vBUg0gF7w-mpiQ&usqp=CAU'}/>
             <div className="profile-text">
-                <h2>{user.username}</h2>
+                <h2>{search.username}</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus erat ac mauris volutpat facilisis. Fusce malesuada ex in nisl ornare, at ornare augue condimentum. Nullam mattis nisi id dui.</p>   
             </div>
         </div>
 
-        <div className="comics>">
-            <h3>{`${user.username}'s comics`}</h3>
+        {/* <div className="comics>">
+            <h3>{`${search.username}'s comics`}</h3>
             <div className="comics-by-category">{comics.map(comic=><ComicsCard comic={comic}/>)}</div>
             
-        </div>
+        </div> */}
     </div> 
     :
     <h1>Loading...</h1>
